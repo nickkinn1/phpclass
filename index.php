f7c2db738942204d07c2934e4d13b7b9854072a3
Nick <300103812@fvtc.edu>
2025-09-05 15:09:01 -0500

    Updated Public Website

--- index.php
+++ index.php
@@ -6,8 +6,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Nick's Website</title>
+    <link rel="stylesheet" href="css/base.css">
 </head>
 <body>
-    <h1>Hello World</h1>
+    <header>
+        <h1>PHP Class - Nick's Website</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/#">Loops</a></li>
+            <li><a href="/#">Counters</a></li>
+            <li><a href="/#">Other</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <img src="images/nick.jpg" alt="Profile Picture">
+        <p>Studying for Web Development and Design.</p>
+    </main>
+
+    <footer>
+        <p>&copy; NickKinney.com</p>
+    </footer>
 </body>
 </html>
